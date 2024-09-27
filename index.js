7754291ece9a93e66e6b87ceff4856f1d65cb4d0
joseja <joseja8@gmail.com>
2024-09-27 11:37:50 +0200

    Adapted for data-ml-serverless-stack

--- index.js
+++ index.js
@@ -13,54 +13,54 @@ const _ = require('lodash');
 BbPromise.promisifyAll(fse);
 
 class ServerlessAWSBatch {
-  constructor(serverless, options) {
-    this.serverless = serverless;
-    this.options = options;
-    this.provider = this.serverless.getProvider('aws');
+    constructor(serverless, options) {
+        this.serverless = serverless;
+        this.options = options;
+        this.provider = this.serverless.getProvider('aws');
 
-    // Make sure that we add the names for our ECR, docker, and batch resources to the provider
-    _.merge(
-      this.provider.naming,
-      {
-        'getECRLogicalId': ecr.getECRLogicalId,
-        'getECRRepositoryName': ecr.getECRRepositoryName,
-        'getECRRepositoryURL': ecr.getECRRepositoryURL,
-        'getDockerImageName': docker.getDockerImageName,
-        'getBatchServiceRoleLogicalId': batchenvironment.getBatchServiceRoleLogicalId,
-        'getBatchInstanceManagementRoleLogicalId': batchenvironment.getBatchInstanceManagementRoleLogicalId,
-        'getBatchInstanceManagementProfileLogicalId': batchenvironment.getBatchInstanceManagementProfileLogicalId,
-        'getBatchSpotFleetManagementRoleLogicalId': batchenvironment.getBatchSpotFleetManagementRoleLogicalId,
-        'getBatchJobExecutionRoleLogicalId': batchtask.getBatchJobExecutionRoleLogicalId,
-        'getLambdaScheduleExecutionRoleLogicalId': batchenvironment.getLambdaScheduleExecutionRoleLogicalId,
-        'getBatchComputeEnvironmentLogicalId': batchenvironment.getBatchComputeEnvironmentLogicalId,
-        'getBatchJobQueueLogicalId': batchenvironment.getBatchJobQueueLogicalId,
-        'getBatchJobQueueName': batchenvironment.getBatchJobQueueName,
-        'getJobDefinitionLogicalId': batchtask.getJobDefinitionLogicalId
-      }
-    );
+        // Make sure that we add the names for our ECR, docker, and batch resources to the provider
+        _.merge(
+            this.provider.naming,
+            {
+                'getECRLogicalId': ecr.getECRLogicalId,
+                'getECRRepositoryName': ecr.getECRRepositoryName,
+                'getECRRepositoryURL': ecr.getECRRepositoryURL,
+                'getDockerImageName': docker.getDockerImageName,
+                'getBatchServiceRoleLogicalId': batchenvironment.getBatchServiceRoleLogicalId,
+                'getBatchInstanceManagementRoleLogicalId': batchenvironment.getBatchInstanceManagementRoleLogicalId,
+                'getBatchInstanceManagementProfileLogicalId': batchenvironment.getBatchInstanceManagementProfileLogicalId,
+                'getBatchSpotFleetManagementRoleLogicalId': batchenvironment.getBatchSpotFleetManagementRoleLogicalId,
+                'getBatchJobExecutionRoleLogicalId': batchtask.getBatchJobExecutionRoleLogicalId,
+                'getLambdaScheduleExecutionRoleLogicalId': batchenvironment.getLambdaScheduleExecutionRoleLogicalId,
+                'getBatchComputeEnvironmentLogicalId': batchenvironment.getBatchComputeEnvironmentLogicalId,
+                'getBatchJobQueueLogicalId': batchenvironment.getBatchJobQueueLogicalId,
+                'getBatchJobQueueName': batchenvironment.getBatchJobQueueName,
+                'getJobDefinitionLogicalId': batchtask.getJobDefinitionLogicalId
+            }
+        );
 
-    // Define inner lifecycles
-    this.commands = {}
+        // Define inner lifecycles
+        this.commands = {}
 
-    this.hooks = {
-      'after:package:initialize': () => BbPromise.bind(this)
-        .then(generateCoreTemplate.generateCoreTemplate),
+        this.hooks = {
+            'after:package:initialize': () => BbPromise.bind(this)
+                .then(generateCoreTemplate.generateCoreTemplate),
 
-      'before:package:compileFunctions': () => BbPromise.bind(this)
-        .then(batchenvironment.validateAWSBatchServerlessConfig)
-        .then(batchenvironment.generateAWSBatchTemplate)
-        .then(batchtask.compileBatchTasks),
+            'before:package:compileFunctions': () => BbPromise.bind(this)
+                .then(batchenvironment.validateAWSBatchServerlessConfig)
+                .then(batchenvironment.generateAWSBatchTemplate)
+                .then(batchtask.compileBatchTasks),
 
-      'after:package:createDeploymentArtifacts': () => BbPromise.bind(this)
-        .then(docker.buildDockerImage),
+            'after:package:createDeploymentArtifacts': () => BbPromise.bind(this)
+                .then(docker.buildDockerImage),
 
-      'after:aws:deploy:deploy:updateStack': () => BbPromise.bind(this)
-        .then(docker.pushDockerImageToECR),
+            'after:aws:deploy:deploy:updateStack': () => BbPromise.bind(this)
+                .then(docker.pushDockerImageToECR),
 
-      'before:remove:remove': () => BbPromise.bind(this)
-        .then(awscli.deleteAllDockerImagesInECR)
+            'before:remove:remove': () => BbPromise.bind(this)
+                .then(awscli.deleteAllDockerImagesInECR)
+        }
     }
-  }
 }
 
 module.exports = ServerlessAWSBatch;
